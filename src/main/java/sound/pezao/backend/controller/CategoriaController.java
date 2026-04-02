dc4e6ebe246aef7a51f65a3a47339eb07fca3285
IsaakGuilherme <isaak.guilherme@sptech.school>
2026-04-02 18:45:17 -0300

    feat: criado o endpont para cadastrar nova categoria

--- src/main/java/sound/pezao/backend/controller/CategoriaController.java
+++ src/main/java/sound/pezao/backend/controller/CategoriaController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import sound.pezao.backend.dto.categoriaDTO.CategoriaRequest;
 import sound.pezao.backend.dto.categoriaDTO.CategoriaResponse;
 import sound.pezao.backend.service.CategoriaService;
 
@@ -26,5 +29,10 @@ public class CategoriaController {
     @Operation(summary = "Lista todas as categorias")
     public ResponseEntity<List<CategoriaResponse>> findAll(){
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaResponse> create(@RequestBody CategoriaRequest request){
+        return ResponseEntity.status(201).body(service.create(request));
     }
 }
