60845ef8ce49c3fabf616d0b06266d029218c4e3
IsaakGuilherme <isaak.guilherme@sptech.school>
2026-04-02 22:30:34 -0300

    feat: criado endpoint para atualizar e deletar categorias

--- src/main/java/sound/pezao/backend/dto/categoriaDTO/CategoriaResponse.java
+++ src/main/java/sound/pezao/backend/dto/categoriaDTO/CategoriaResponse.java
@@ -3,7 +3,19 @@ package sound.pezao.backend.dto.categoriaDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Dados retornados de uma categoria")
-public record CategoriaResponse(
+public class CategoriaResponse {
     @Schema(description = "Nome da categoria", example = "Som automotivo")
-    String name
-) {}
+    private String nome;
+
+    public CategoriaResponse(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
