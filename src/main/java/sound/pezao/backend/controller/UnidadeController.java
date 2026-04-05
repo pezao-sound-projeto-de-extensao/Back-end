289c2f3ac7eddf8c23990911587c7c886bcdddb8
IsaakGuilherme <isaak.guilherme@sptech.school>
2026-04-05 13:53:59 -0300

    remove: remoção do endpoint de buscar unidade por id

--- src/main/java/sound/pezao/backend/controller/UnidadeController.java
+++ src/main/java/sound/pezao/backend/controller/UnidadeController.java
@@ -3,12 +3,18 @@ package sound.pezao.backend.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import sound.pezao.backend.dto.unidadesDTO.UnidadeRequest;
 import sound.pezao.backend.dto.unidadesDTO.UnidadeResponse;
 import sound.pezao.backend.service.UnidadeService;
 
@@ -23,12 +29,27 @@ public class UnidadeController {
     }
 
     @GetMapping
-    @Operation(description = "Retorna uma lista de todas as unidades de medida disponíveis.")
+    @Operation(summary = "Retorna uma lista de todas as unidades de medida disponíveis.")
     public ResponseEntity<List<UnidadeResponse>> findAll(){
         return ResponseEntity.ok(service.findAll());
     }
 
-    public ResponseEntity<UnidadeResponse> findById(Integer id){
-        return ResponseEntity.ok(service.findById(id));
+    @PostMapping
+    @Operation(summary = "Cria uma nova unidade de medida com os dados fornecidos.")
+    public ResponseEntity<UnidadeResponse> create(@RequestBody UnidadeRequest request){
+        return ResponseEntity.status(201).body(service.create(request));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualiza os detalhes de uma unidade de medida existente com base no ID fornecido e nos dados atualizados.")
+    public ResponseEntity<UnidadeResponse> update(@PathVariable Integer id, @RequestBody UnidadeRequest request){
+        return ResponseEntity.ok(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui uma unidade de medida específica com base no ID fornecido.")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
