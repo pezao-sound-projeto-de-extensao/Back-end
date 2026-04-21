ca27fd799585df860c774bd903f6401e5af34298
HerculesSP <hercules.pereira@sptech.school>
2026-04-21 16:46:35 -0300

    feat: Endpoint de health adicionado com sucesso

---
+++ src/main/java/sound/pezao/backend/controller/HealthController.java
@@ -0,0 +1,20 @@
+package sound.pezao.backend.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@Tag(name = "Health", description = "Verifica a saúde da aplicação")
+public class HealthController {
+
+    @GetMapping
+    @Operation(summary = "Informar a saúde da aplicação")
+    public ResponseEntity<Void> health(){
+        return ResponseEntity.ok().build();
+    }
+}
