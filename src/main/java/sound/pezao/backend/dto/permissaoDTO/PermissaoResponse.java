39be7316be0b1e46c15b586e0d84175b88fc362f
Amanda-Oliveira18 <amanda.silva@sptech.school>
2026-05-24 20:07:58 -0300

    feat: adiciona testes de service e controller de permissões

--- src/main/java/sound/pezao/backend/dto/permissaoDTO/PermissaoResponse.java
+++ src/main/java/sound/pezao/backend/dto/permissaoDTO/PermissaoResponse.java
@@ -1,7 +1,11 @@
 package sound.pezao.backend.dto.permissaoDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PermissaoResponse(
+    @Schema(description = "nome da permissão", example = "cadastrar_itens")
     String nome,
+    @Schema(description = "descrição da permissão", example = "Cadastrar novos itens no estoque")
     String descricao
 ) {
 }
